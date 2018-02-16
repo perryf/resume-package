bcefc2aed32331c40164170917f5b06f89b32973
Perry Fustero <pjfust@gmail.com>
2018-02-16 16:42:42 -0500

    make even more awesome

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 #! /usr/bin/env node
 
 const fs = require('fs')
-const colors = require('colors')
+const cfonts = require('cfonts');
 
 fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
     if (err) {
         console.log(err)
     } else {
-        // let perry = JSON.parse(data)
-        // console.log(perry.name)
-        console.log(data.rainbow.italic.bgBlack)
+        let perry = JSON.parse(data)
+        printPerry(perry)
         return data
     }
 })
+
+function printPerry(obj) {
+
+    cfonts.say(`${obj.firstName}|${obj.lastName}`, {
+        font: 'block',
+        align: 'center',
+        colors: ['blue', 'candy']
+    })
+    cfonts.say(`
+        ${obj.email}|
+        ${obj.website}|
+        github: ${obj.github}|
+        resume: ${obj.resume}|
+        linkedin: ${obj.linkedin}|
+        skills: ${obj.proficientTechnologies},${obj.otherSkills}
+        `, {
+            font: 'console',
+            align: 'center',
+            colors: ['magenta'],
+            space: false,
+            letterSpacing: 0
+        }
+    )
+}
