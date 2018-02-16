ddc0d45adfdfec56141bad63474ce080ee7779fe
Perry Fustero <pjfust@gmail.com>
2018-02-16 09:59:01 -0500

    add colors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 #! /usr/bin/env node
 
 const fs = require('fs')
+const colors = require('colors')
 
 fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
     if (err) {
         console.log(err)
     } else {
-        console.log(data)
+        // let perry = JSON.parse(data)
+        // console.log(perry.name)
+        console.log(data.rainbow.italic.bgBlack)
         return data
     }
 })
